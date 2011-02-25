596fcb7b1b7684a8cf946f3ac98db33a3a80128b
Znow <znowm4n@gmail.com>
2011-02-25 14:05:29 +0100

    update tests

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,5 @@
 class StoreController < ApplicationController
+  
   def index
     @products = Product.all # Gets all the products from Products table and adds them to instance variable @products
   end
