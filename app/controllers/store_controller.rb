71f5c61302f1132c77060bc0235789e3cffe780d
Znow <znowm4n@gmail.com>
2011-02-25 11:40:41 +0100

    updated layout

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,6 @@
+class StoreController < ApplicationController
+  def index
+    @products = Product.all # Gets all the products from Products table and adds them to instance variable @products
+  end
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
+  # validation
   validates :title, :description, :image_url, :presence => true # :title, :description, :image_url has to be present/not empty!
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01} # validates that the price contains a valid number and is not below a price of 0.01
   validates :title, :uniqueness => true # validates that the :title is unique
