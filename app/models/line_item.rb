5151099dcf3c212d391e5031c7cfe4c93f120ab7
Znow <znowm4n@gmail.com>
2011-03-03 14:58:34 +0100

    update

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product #rows in lineitem is children of product and cart
   belongs_to :cart
+  
+  def total_price
+    product.price * quantity
+  end
 end
