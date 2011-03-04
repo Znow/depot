3b29e17932418dcde116a634b8ba5512c9569d24
Znow <znowm4n@gmail.com>
2011-03-04 12:33:03 +0100

    fixed errors

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,6 +47,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to(store_url) }
+        format.js # renders a js template names create.js.rjs
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
