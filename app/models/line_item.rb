5a9b6505ebee12d9c537dbd80141b41cb5ff1a2a
Znow <znowm4n@gmail.com>
2011-05-30 19:55:13 +0200

    update

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,5 @@
 class LineItem < ActiveRecord::Base
+  belongs_to :order
   belongs_to :product #rows in lineitem is children of product and cart
   belongs_to :cart
   
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, :dependent => :destroy # When order is finished, destroy the line_item, the actual order
+  validates :name, :address, :email, :pay_type, :presence => true
+  validates :pay_type, :inclusion => PAYMENT_TYPES
+
+  PAYMENT_TYPES = ["Check", "Credit Card", "Purchase Order"]
 end
